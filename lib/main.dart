baded5bd3358dd2e99b6fae047d08c439da4e8cc
Bs Vishnu Vardhan <vishnuvardhanbs.work@gmail.com>
2026-03-14 13:21:16 +0530

    phase 2: added live GPS tracking on map

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const MyApp());
+void main() {
+  runApp(const AmbulanceApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AmbulanceApp extends StatelessWidget {
+  const AmbulanceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Firebase Connected 🚑"),
+      home: MapScreen(),
+    );
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+
+  GoogleMapController? mapController;
+  LatLng currentPosition = const LatLng(0, 0);
+
+  Future<void> getLocation() async {
+
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    setState(() {
+      currentPosition = LatLng(position.latitude, position.longitude);
+    });
+
+    mapController?.animateCamera(
+      CameraUpdate.newLatLngZoom(currentPosition, 16),
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Ambulance Tracker")),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(17.385044, 78.486671),
+          zoom: 12,
         ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("ambulance"),
+            position: currentPosition,
+          )
+        },
       ),
     );
   }
